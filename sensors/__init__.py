d27036a08680c760c493bf5576b21c2fcdc3cfbf
Richard Hull <rm_hull@yahoo.co.uk>
2019-03-09 14:18:52 +0000

    Updated contact information

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -6,10 +6,10 @@ from ctypes.util import find_library
 
 from sensors import stdc
 
-__version__ = '0.0.4'
-__date__ = '2016-08-06'
-__author__ = "Marc 'BlackJack' Rintsch"
-__contact__ = 'marc@rintsch.de'
+__version__ = '0.1.0'
+__date__ = '2019-03-09'
+__author__ = "Richard Hull / Marc 'BlackJack' Rintsch"
+__contact__ = 'richard.hull@destructuring-bind.org'
 __license__ = 'LGPL v2.1'
 
 API_VERSION = 4
@@ -65,7 +65,7 @@ class Subfeature(Structure):
         ('mapping', c_int),
         ('flags', c_uint),
     ]
-    
+
     def __repr__(self):
         return '<%s name=%r number=%d type=%d mapping=%d flags=%08x>' % (
             self.__class__.__name__,
@@ -75,7 +75,7 @@ class Subfeature(Structure):
             self.mapping,
             self.flags
         )
-    
+
     def get_value(self):
         result = c_double()
         _get_value(byref(self.parent.chip), self.number, byref(result))
@@ -92,7 +92,7 @@ class Feature(Structure):
         ('_first_subfeature', c_int),
         ('_padding1', c_int),
     ]
-    
+
     def __repr__(self):
         return '<%s name=%r number=%r type=%r>' % (
             self.__class__.__name__,
@@ -100,7 +100,7 @@ class Feature(Structure):
             self.number,
             self.type
         )
-    
+
     def __iter__(self):
         number = c_int(0)
         while True:
@@ -115,18 +115,18 @@ class Feature(Structure):
             result.chip = self.chip
             result.parent = self
             yield result
-    
+
     @property
     def label(self):
         ptr = _get_label(byref(self.chip), byref(self))
         result = string_at(ptr)
         stdc.free(ptr)
         return result
-    
+
     def get_value(self):
-        # 
+        #
         # TODO Is the first always the correct one for all feature types?
-        # 
+        #
         return iter(self).next().get_value()
 
 FEATURE_P = POINTER(Feature)
@@ -135,21 +135,21 @@ FEATURE_P = POINTER(Feature)
 class Bus(Structure):
     TYPE_ANY = -1
     NR_ANY = -1
-    
+
     _fields_ = [
         ('type', c_short),
         ('nr', c_short),
     ]
-    
+
     def __str__(self):
         return (
             '*' if self.type == self.TYPE_ANY
                 else _get_adapter_name(byref(self))
         )
-    
+
     def __repr__(self):
         return '%s(%r, %r)' % (self.__class__.__name__, self.type, self.nr)
-    
+
     @property
     def has_wildcards(self):
         return self.type == self.TYPE_ANY or self.nr == self.NR_ANY
@@ -158,38 +158,38 @@ BUS_P = POINTER(Bus)
 
 
 class Chip(Structure):
-    # 
+    #
     # TODO Move common stuff into `AbstractChip` class.
-    # 
+    #
     _fields_ = [
         ('prefix', c_char_p),
         ('bus', Bus),
         ('addr', c_int),
         ('path', c_char_p),
     ]
-    
+
     PREFIX_ANY = None
     ADDR_ANY = -1
-    
+
     def __new__(cls, *args):
         result = super(Chip, cls).__new__(cls)
         if args:
             _parse_chip_name(args[0], byref(result))
         return result
-        
+
     def __init__(self, *_args):
         Structure.__init__(self)
-        # 
+        #
         # Need to bind the following to the instance so it is available in
         #  `__del__()` when the interpreter shuts down.
-        # 
+        #
         self._free_chip_name = _free_chip_name
         self.byref = byref
-    
+
     def __del__(self):
         if self._b_needsfree_:
             self._free_chip_name(self.byref(self))
-    
+
     def __repr__(self):
         return '<%s prefix=%r bus=%r addr=%r path=%r>' % (
             (
@@ -200,14 +200,14 @@ class Chip(Structure):
                 self.path
             )
         )
-    
+
     def __str__(self):
         buffer_size = 200
         result = create_string_buffer(buffer_size)
         used = _snprintf_chip_name(result, len(result), byref(self))
         assert used < buffer_size
         return result.value
-    
+
     def __iter__(self):
         number = c_int(0)
         while True:
@@ -217,11 +217,11 @@ class Chip(Structure):
             result = result_p.contents
             result.chip = self
             yield result
-    
+
     @property
     def adapter_name(self):
         return str(self.bus)
-    
+
     @property
     def has_wildcards(self):
         return (
@@ -260,10 +260,10 @@ _get_value.argtypes = [CHIP_P, c_int, POINTER(c_double)]
 _get_value.restype = c_int
 _get_value.errcheck = _error_check
 
-# 
+#
 # TODO sensors_set_value()
 # TODO sensors_do_chip_sets()
-# 
+#
 
 _get_detected_chips = SENSORS_LIB.sensors_get_detected_chips
 _get_detected_chips.argtypes = [CHIP_P, POINTER(c_int)]
@@ -277,9 +277,9 @@ _get_all_subfeatures = SENSORS_LIB.sensors_get_all_subfeatures
 _get_all_subfeatures.argtypes = [CHIP_P, FEATURE_P, POINTER(c_int)]
 _get_all_subfeatures.restype = SUBFEATURE_P
 
-# 
+#
 # TODO sensors_get_subfeature() ?
-# 
+#
 
 def iter_detected_chips(chip_name='*-*'):
     chip = Chip(chip_name)
